53eda667720194b1aa59c59a741d8cec74d1dcac
Quang Huy Vu <39373215+vuhuycto@users.noreply.github.com>
2022-08-11 10:33:53 +0000

    refactor: Extract variables

--- mini-project-3/main.py
+++ mini-project-3/main.py
@@ -1,15 +1,25 @@
 from random import choice
 
+rock = "đấm"
+scissor = "kéo"
+paper = "lá"
+
+
 def find_winner(player_choice, computer_choice):
-    if (player_choice == "đấm" and computer_choice == "kéo") or (player_choice == "kéo" and computer_choice == "lá") or (player_choice == "lá" and computer_choice == "đấm"):
+    if (player_choice == rock and computer_choice == scissor) or (player_choice == scissor and computer_choice == paper) or (player_choice == paper and computer_choice == rock):
         return "Người chơi"
-    elif (player_choice == "kéo" and computer_choice == "đấm") or (player_choice == "lá" and computer_choice == "kéo") or (player_choice == "đấm" and computer_choice == "lá"):
+    elif (player_choice == scissor and computer_choice == rock) or (player_choice == paper and computer_choice == scissor) or (player_choice == rock and computer_choice == paper):
         return "Máy"
     else:
         return "Không ai"
 
+
 player_choice = input("Lựa chọn của người chơi (đấm, lá, kéo): ")
-computer_choice = choice(["đấm", "lá", "kéo"])
+if player_choice != rock and player_choice != scissor and player_choice != paper:
+    print("Lựa chọn không hợp lệ!")
+    exit(1)
+
+computer_choice = choice([rock, paper, scissor])
 
 winner = find_winner(player_choice, computer_choice)
 print(f"{winner} thắng")
